fe28056aced7d8baf52e47b8e6a40b7b9ff0ab69
b-trots <bvtrots@gmail.com>
2025-11-18 20:45:07 +0300

    feat (react-route): добавляет роутинг

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import {render} from "react-dom";
 import App from "./App";
+import {BrowserRouter} from "react-router-dom";
 
-render(<App/>,
+render(<BrowserRouter><App/></BrowserRouter>,
     document.getElementById('root'));
