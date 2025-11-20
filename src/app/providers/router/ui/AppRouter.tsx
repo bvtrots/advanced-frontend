69c3b124fb4663f811328f6a8f1f335b1ed18e6c
b-trots <bvtrots@gmail.com>
2025-11-20 23:11:15 +0300

    feat (ui): добавляет Sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -5,14 +5,17 @@ import {routeConfig} from "shared/config/routeConfig/routeConfig";
 const AppRouter = () => {
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                {Object.values(routeConfig).map(({path, element}) => (
-                    <Route
-                        key={path}
-                        path={path}
-                        element={element}/>
-                ))}
-            </Routes>
+            <div className='page-wrapper'>
+
+                <Routes>
+                    {Object.values(routeConfig).map(({path, element}) => (
+                        <Route
+                            key={path}
+                            path={path}
+                            element={element}/>
+                    ))}
+                </Routes>
+            </div>
         </Suspense>
     );
 };
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -6,7 +6,7 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
         cls,
         ...additional.filter(Boolean),
         ...Object.entries(mods)
-            .filter(([cls, value]) => Boolean(value))
-            .map(className => className)]
+            .filter(([_, value]) => Boolean(value))
+            .map(([className]) => className)]
         .join(' ');
 }
