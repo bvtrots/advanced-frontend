087491a07edbc75972cea67816e9ccc3f7a419c9
b-trots <bvtrots@gmail.com>
2025-11-20 21:08:48 +0300

    build (build): добавляет лоадеры svg и файлов

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -5,3 +5,12 @@ declare module '*.scss' {
     const classnames: IClassNames;
     export = classnames;
 }
+
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg" {
+    const SVG: React.VFC<React.SVGProps<SVGElement>>;
+    export default SVG;
+}
