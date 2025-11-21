0811b741a0d0d946f0cb9ad839ce492e24bda34b
b-trots <bvtrots@gmail.com>
2025-11-21 12:39:25 +0300

    feat (ui): добавляет языковую локализацию

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -2,6 +2,7 @@ declare module '*.scss' {
     interface IClassNames {
         [className: string]: string
     }
+
     const classnames: IClassNames;
     export = classnames;
 }
@@ -14,3 +15,6 @@ declare module "*.svg" {
     const SVG: React.VFC<React.SVGProps<SVGElement>>;
     export default SVG;
 }
+
+
+declare const __IS_DEV__: boolean;
