6449b15f2913dfd0656e3d51200989df99c2f111
b-trots <bvtrots@gmail.com>
2025-11-22 10:30:51 +0300

    test: добавляет jest и тесты classNames

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,8 +1,8 @@
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BuildOptions } from './types/config';
+import {BuildOptions} from './types/config';
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
@@ -25,6 +25,26 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         ],
     };
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        },
+                    ]
+                ]
+            }
+        }
+    }
+
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -43,6 +63,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ];
--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,5 +1,3 @@
-import React from 'react';
-
 declare module '*.scss' {
     interface IClassNames {
         [className: string]: string
@@ -12,8 +10,10 @@ declare module '*.scss' {
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
+
 declare module '*.svg' {
-    const SVG: React.VFC<React.SVGProps<SVGElement>>;
+    import React from 'react';
+    const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
     export default SVG;
 }
 
