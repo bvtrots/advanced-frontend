ae3597ec6955e2bb54636bcd4ec47193999441a6
b-trots <bvtrots@gmail.com>
2025-11-27 19:20:38 +0300

    build (build): добавляет BundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,12 +1,13 @@
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BuildOptions } from './types/config';
+import {BuildOptions} from './types/config';
+import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
 
 export function buildPlugins({
-    paths,
-    isDev,
-}: BuildOptions): webpack.WebpackPluginInstance[] {
+                                 paths,
+                                 isDev,
+                             }: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
         new HTMLWebpackPlugin({
             template: paths.html,
@@ -20,5 +21,6 @@ export function buildPlugins({
             __IS_DEV__: JSON.stringify(isDev),
         }),
         new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({openAnalyzer: false}),
     ];
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import App from './app/App';
 import 'shared/config/i18n/i18n';
 import {ErrorBoundary} from "app/providers/ErrorBoundary";
 
+
 render(
     <BrowserRouter>
         <ErrorBoundary>
